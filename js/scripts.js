108759e1417849a53b17e1ff9af2ba04323d4b2b
robbryandev <robbryandev@gmail.com>
2022-10-28 08:35:56 -0700

    Add number form and submission

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,16 @@
 // Buisiness logic
+String.prototype.parseInt = function() {
+  return this.trim() !== "" ? parseInt(this.trim()) : 0;
+}
 
 // User interface logic
+function submitForm(event) {
+  event.preventDefault();
+  console.log(document.getElementById("user-number").value.parseInt());
+}
+
+// Page load
+addEventListener("load", function() {
+  const numberForm = document.querySelector("form");
+  numberForm.addEventListener("submit", submitForm);
+});
