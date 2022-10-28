7fd3e382de853d38bb47590c157283db0cea20bf
robbryandev <robbryandev@gmail.com>
2022-10-28 10:34:57 -0700

    Refactor replace method

--- js/scripts.js
+++ js/scripts.js
@@ -7,10 +7,10 @@
 // And a method to validate if both arrays have the same length.
 class ReplaceObject {
   constructor(numArray, strArray) {
-    this.numArray = numArray.map(function(num) {
+    this.numArray = numArray.map(function (num) {
       return Number(num);
     });
-    this.strArray = strArray.map(function(str) {
+    this.strArray = strArray.map(function (str) {
       return String(str);
     });
   }
@@ -20,50 +20,59 @@ class ReplaceObject {
   }
 }
 
-// RogerArray class takes an array of numbers
-// and a ReplaceObject containing the rules for how the numbers should be replaced.
+// RogerArray class takes an array of numbers.
+// A ReplaceObject containing the rules for how the numbers should be replaced.
+// And a replace method to conditionally replace numbers in the number array with strings provided.
 class RogerArray {
-  constructor(numCountArray, replaceObject) {
-    this.numCountArray = numCountArray.map(function(num) {
+  constructor(numCountArray, repObject) {
+    this.numCountArray = numCountArray.map(function (num) {
       return Number(num);
     });
-    this.replaceObject = typeof replaceObject === ReplaceObject ? replaceObject : ReplaceObject([0], [""]);
-  }
-
-  replace() {
-    return this.numCountArray.map(function(num) {
-      const index = num;
-      if (num === this.replaceObject.numArray[index]) {
-        return this.replaceObject.strArray[index];
-      }
-    });
+    this.repObject =
+      typeof repObject === "object" ? repObject : new ReplaceObject([0], [""]);
   }
 }
+
+RogerArray.prototype.replace = function () {
+  let self = this;
+  let res = [];
+  this.numCountArray.forEach(function (num) {
+    const rep = self.repObject;
+    if (rep.numArray.includes(num)) {
+      res.push(rep.strArray[rep.numArray.indexOf(num)]);
+    } else {
+      res.push(num);
+    }
+  });
+  return res;
+};
 
 // Methods for Standard objects
 
 // String method that returns provided string as a number or 0 instead of NaN
-String.prototype.parseInt = function() {
+String.prototype.parseInt = function () {
   return !Number(this.trim()) ? 0 : parseInt(this.trim());
-}
+};
 
 // Number method that gets an array from 0 to the number provided
-Number.prototype.countArray = function() {
+Number.prototype.countArray = function () {
   let res = [];
   for (let i = 0; i <= this; i++) {
     res.push(i);
   }
   return res.length > 0 ? res : [0];
-}
+};
 
 // User interface logic
 function submitForm(event) {
   event.preventDefault();
-  console.log(document.getElementById("user-number").value.parseInt().countArray());
+  console.log(
+    document.getElementById("user-number").value.parseInt().countArray()
+  );
 }
 
 // Page load
-addEventListener("load", function() {
+addEventListener("load", function () {
   const numberForm = document.querySelector("form");
   numberForm.addEventListener("submit", submitForm);
 });
