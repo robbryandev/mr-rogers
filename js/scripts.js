b03a23c32fb12d685b29ac11a88091ebf8ce3bf4
robbryandev <robbryandev@gmail.com>
2022-10-28 09:00:28 -0700

    Structure data classes

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,46 @@ String.prototype.parseInt = function() {
   return this.trim() !== "" ? parseInt(this.trim()) : 0;
 }
 
+Number.prototype.countArray = function() {
+  let res = [];
+  for (let i = 0; i <= this; i++) {
+    res.push(i);
+  }
+  return res;
+}
+
+class ReplaceObject {
+  constructor(numArray, strArray) {
+    this.numArray = numArray.map(function(num) {
+      return Number(num);
+    });
+    this.strArray = strArray.map(function(str) {
+      return String(str);
+    });
+  }
+
+  isValid() {
+    return this.numArray.length === this.strArray.length;
+  }
+}
+
+class RogerArray {
+  constructor(numCountArray, replaceObject) {
+    this.numCountArray = numCountArray.map(function(num) {
+      return Number(num);
+    });
+    this.replaceObject = typeof replaceObject === ReplaceObject ? replaceObject : ReplaceObject([0], [""]);
+  }
+
+  replace() {
+    
+  }
+}
+
 // User interface logic
 function submitForm(event) {
   event.preventDefault();
-  console.log(document.getElementById("user-number").value.parseInt());
+  console.log(document.getElementById("user-number").value.parseInt().countArray());
 }
 
 // Page load
